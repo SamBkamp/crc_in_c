f00fcbb27ce4d55670442d8b5221bf3758a21673
Sam Bonnekamp <sam@bonnekamp.net>
2025-05-02 16:37:40 +0800

    added initial values and xorout

--- crc.c
+++ crc.c
@@ -3,10 +3,13 @@
 #include <stdlib.h>
 #include <limits.h>
 
-#define CRC_LEN 32
-#define generator 2168537515
+#define CRC_LEN 16
+#define GENERATOR 0x1021
+#define INIT_VAL 0xFFFF
+#define XOR_OUT 0xFFFF
 
-z
+
+
 //PRINTING/FORMATTING FUNCTIONS
 void pr(char* t, size_t len){ //print string with binary representation
   size_t bufSize = CHAR_BIT+1;
@@ -50,37 +53,39 @@ unsigned long* generateCRC(char* t){
   //SHIFTING INTO REGISTER
   for (int i = 0; i < chars; i++){
     for (int j = CHAR_BIT-1; j >= 0; j--){
+      //NTH ITERATION: CHAR_BIT-j + (i * CHAR_BIT)
       *sRegister <<= 1; //shift register
       unsigned long MSB = (t[i] & msb_mask ? 1 : 0); //get next bit in stream
       *sRegister |= MSB; //add next bit to register
       t[i] <<= 1; //shift stream to next bit
+      if(CHAR_BIT-j + (i * CHAR_BIT) == CRC_LEN)
+	*sRegister ^= INIT_VAL;
       unsigned long a = (shift_reg_mask & *sRegister) >> CRC_LEN;  // wether or not MSB is 1
-      *sRegister = *sRegister ^ (a ? generator : 0); //xor with the generator if MSB is 1, otherwise xor with 0 (do nothing)
+      *sRegister = *sRegister ^ (a ? GENERATOR : 0); //xor with the GENERATOR if MSB is 1, otherwise xor with 0 (do nothing)
     }
   }
+  *sRegister = *sRegister ^ XOR_OUT; //xor out
   return sRegister;  
 }
 
 int main(int argc, char* argv[]){
 
-  char t[6];
+  char t[4];
   t[0] = 0x41;
-  t[1] = 0x41;
+  t[1] = 0x42;
   t[2] = 0;
   t[3] = 0; //MAKE SURE YOU HAVE AT LEAST AS MANY NULL TERMINATORS AS YOU HAVE BYTES IN THE CRC
-  t[4] = 0;
-  t[5] = 0;
     
   pr(t, sizeof(t) - 1); //WARNING YOU SHOULD NOT SUBTRACT FROM A SIZE_T UNLESS U WANT SOMETHING TO BREAK LATER
   unsigned long* crc = generateCRC(t);
 
   *crc &= ULONG_MAX ^ (ULONG_MAX << CRC_LEN); //mask away any unused bits that are still technically part of the int (bc we're only using a small part of the int)
-  printf("\ngenerator: %lu\n", generator);
+  printf("\ngenerator: %lx\n", GENERATOR);
   printf("generator in bin: ");
-  printCharAsBinary(generator, CRC_LEN);  
+  printCharAsBinary(GENERATOR, CRC_LEN);  
   
   
-  printf("\nresult: %lu\n", *crc);
+  printf("\nresult: %lx\n", *crc);
   printf("result in bin: ");
   printCharAsBinary(*crc, CRC_LEN);
   
